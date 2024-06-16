a4c62f39e01688a44e2464a127b8b66a5707bb88
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-16 08:17:41 -0700

    measures height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (NULL);
+		return (0);
 
 	left_tree = binary_tree_height(tree->left);
 	right_tree = binary_tree_height(tree->right);
