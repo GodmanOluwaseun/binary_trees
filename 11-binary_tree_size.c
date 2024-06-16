05b3144f9585268bd8e4c6ff2220ee765b23383b
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-16 09:03:51 -0700

    Function that measures the size of a binary tree.

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,13 +11,13 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t tree_size;
+	size_t left_tree_size, right_tree_size;
 
 	if (tree == NULL)
 		return (0);
 
-	tree_size = binary_tree_size(tree->left);
-	tree_size = binary_tree_size(tree->right);
+	left_tree_size = binary_tree_size(tree->left);
+	right_tree_size = binary_tree_size(tree->right);
 
-	return (tree_size + 1);
+	return ((left_tree_size + right_tree_size) + 1);
 }
