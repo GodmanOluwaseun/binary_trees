b8778c7842e41a9b83959277f6ff7ca846df5548
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-17 03:13:16 -0700

    Finds balance of tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,12 +11,16 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int bal, left_tree, right_tree;
+	int bal;
+	int left_tree = 0;
+	int right_tree = 0;
 
 	if (tree == NULL)
 		return (0);
 
+	left_tree += 1;
 	left_tree = binary_tree_balance(tree->left);
+	right_tree += 1;
 	right_tree = binary_tree_balance(tree->right);
 
 	bal = left_tree - right_tree;
